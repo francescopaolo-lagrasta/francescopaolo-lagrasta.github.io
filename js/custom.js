82998ff54bf58576a341578a677c393629afbace
Francesco Paolo Lagrasta <francescopaolo.lagrasta@gmail.com>
2024-02-21 22:29:44 +0100

    Adding CV

--- js/custom.js
+++ js/custom.js
@@ -2,6 +2,7 @@ const observer = new IntersectionObserver(entries => {
     // Loop over the entries
     entries.forEach(entry=> {
       // If the element is visible
+      console.log(entry)
       if (entry.isIntersecting) {
         // Add the animation class
         entry.target.classList.add('marx');
